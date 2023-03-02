4a76db9330a03597b85ab8a85a614c36e7709d06
Emanuel Costa <emanuelldsc@gmail.com>
2023-03-01 22:18:31 -0300

    favourites list

--- lib/src/models/dashboard/categories_models_popular.dart
+++ lib/src/models/dashboard/categories_models_popular.dart
@@ -49,45 +49,54 @@ class PopularListWidget extends StatelessWidget {
       width: double.infinity,
       child: ListView.builder(
         itemCount: popular.length,
-        itemBuilder: ((context, index) => InkWell(
-            onTap: () {
-              Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                      builder: (context) => Description(
-                            name: popular[index]['title'],
-                            bannerurl: 'https://image.tmdb.org/t/p/w500' +
-                                popular[index]['backdrop_path'],
-                            posterurl: 'https://image.tmdb.org/t/p/w500' +
-                                popular[index]['poster_path'],
-                            description: popular[index]['overview'],
-                            vote: popular[index]['vote_average'].toDouble(),
-                            launch_on: popular[index]['release_date'],
-                            onTab: () {
-                              if (FavoritesScreen.favoritesList.contains(popular[index]['title'])) {
-                                var snackBar = mySnackBar(isDarkMode, tPrimaryColor, tPrimaryDarkColor, 'ALREADY ON FAVORITES');
-                                ScaffoldMessenger.of(context)
-                                  .showSnackBar(snackBar);
-                              }
-                              else{
-                                FavoritesScreen.favoritesList.add(
-                                popular[index]['title'].toString(),
-                              );
-                                var snackBar = mySnackBar(isDarkMode, tSecundaryColor, tSecundaryDarkColor, 'ADDED TO FAVORITES');
-                              ScaffoldMessenger.of(context)
-                                  .showSnackBar(snackBar);
-                              }                           
-                            },
-                          )));
-            },
-            child: MovieCards(
-                imageName: 'https://image.tmdb.org/t/p/w500' +
-                    popular[index]['poster_path']))),
+        itemBuilder: (context, index) {
+          var title = popular[index]['title'].toString();
+          return InkWell(
+              onTap: () {
+                Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) => Description(
+                              name: popular[index]['title'],
+                              bannerurl: 'https://image.tmdb.org/t/p/w500' +
+                                  popular[index]['backdrop_path'],
+                              posterurl: 'https://image.tmdb.org/t/p/w500' +
+                                  popular[index]['poster_path'],
+                              description: popular[index]['overview'],
+                              vote: popular[index]['vote_average'].toDouble(),
+                              launch_on: popular[index]['release_date'],
+                              onTab: () {
+                                if (FavoritesScreen.favoritesList
+                                    .contains(title)) {
+                                  var snackBar = mySnackBar(
+                                      isDarkMode,
+                                      tPrimaryColor,
+                                      tPrimaryDarkColor,
+                                      'ALREADY ON FAVORITES');
+                                  ScaffoldMessenger.of(context)
+                                      .showSnackBar(snackBar);
+                                } else {
+                                  FavoritesScreen.favoritesList.add(
+                                    title,
+                                  );
+                                  var snackBar = mySnackBar(
+                                      isDarkMode,
+                                      tSecundaryColor,
+                                      tSecundaryDarkColor,
+                                      'ADDED TO FAVORITES');
+                                  ScaffoldMessenger.of(context)
+                                      .showSnackBar(snackBar);
+                                }
+                              },
+                            )));
+              },
+              child: MovieCards(
+                  imageName: 'https://image.tmdb.org/t/p/w500' +
+                      popular[index]['poster_path']));
+        },
         clipBehavior: Clip.none,
         scrollDirection: Axis.horizontal,
       ),
     );
   }
-
-  
 }
